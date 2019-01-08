489c162320b1880609c5f141e3b22d117990a045
Andy Edwards <jedwards@fastmail.com>
2019-01-08 13:29:03 -0600

    fix: tolerate weird distance units, but flag errors

--- test/parseRawSurveyTest.js
+++ test/parseRawSurveyTest.js
@@ -19,7 +19,6 @@ describe('parseRawSurvey', () => {
     const line = ' NT49 NT48  95.8     154 154.5    3  3.5 20 12'
     parseLine(line)
 
-    // expect(errors).to.deep.equal([])
     expect(shots).to.deep.equal([
       {
         file: 'blah.txt',
@@ -38,6 +37,27 @@ describe('parseRawSurvey', () => {
         r: 12,
       },
     ])
+    expect(errors).to.have.lengthOf(2)
+    expect(errors[0].type).to.equal('missing-up')
+    expect(errors[1].type).to.equal('missing-down')
+  })
+  it(`tolerates invalid character after F`, function () {
+    const emitter = new EventEmitter()
+    const parseLine = parseRawSurvey(emitter, 'blah.txt')
+
+    const trips = []
+    const errors = []
+    emitter.on('trip', trip => trips.push(trip))
+    emitter.on('error', error => errors.push(error))
+
+    parseLine('     Fisher Ridge Cave System')
+    parseLine('Test Trip')
+    parseLine(' *')
+    parseLine('F* CC DD')
+
+    expect(trips).to.have.lengthOf(1)
+    expect(trips[0].distUnit).to.equal('F*')
+    expect(errors).to.have.lengthOf(1)
+    expect(errors[0].type).to.equal('invalid-distance-unit')
   })
 })
-
